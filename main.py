58257bb166c3628b0807ea52b5e5d7dff191886b
MINKYU-JANG <jmk1300@hotmail.com>
2024-04-02 20:35:39 +0900

    toppage.html 생성, main.py 수정

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -8,3 +9,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 
 db = SQLAlchemy(app)
+
+
+@app.route("/toppage/")
+def toppage():
+    return render_template('toppage.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
