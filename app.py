4ab68bac2c73375e94464c5c476d278ea920dc41
MINKYU-JANG <jmk1300@hotmail.com>
2024-04-04 08:00:54 +0900

    toppage.html, main.py 수정

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -14,6 +14,17 @@ db = SQLAlchemy(app)
 @app.route("/toppage/")
 def toppage():
     return render_template('toppage.html')
+
+
+@app.route('/delete/<int:noticeID>')
+def delete(noticeID):
+    notice = noticeTable.query.get_or_404(noticeID)
+    question_id = notice.question.id
+
+    db.session.delete(notice)
+    db.session.commit()
+    
+    return redirect(url_for('question.detail', question_id=question_id))
 
 
 if __name__ == "__main__":
